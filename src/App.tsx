6d54d5e8ad51f444abaccf1369bd07e1b2277dda
hatae94 <gkxo5959@naver.com>
2025-08-13 16:34:18 +0900

    feat: eslint, prettier, tailwindcss, custom icon 초기환경 구축

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-medium underline">Hello world!</h1>
+      <h1 className="font-regular text-3xl underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
